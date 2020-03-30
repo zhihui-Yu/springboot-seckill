b7aeeb01b728478239d6ccceccbbd515c9086214
zhihui-Yu <54210717+zhihui-Yu@users.noreply.github.com>
2020-03-30 19:13:46 +0800

    添加 秒杀业务

---
+++ server/src/main/java/com/sk/server/controller/KillController.java
@@ -0,0 +1,67 @@
+package com.sk.server.controller;
+
+import com.sk.api.enums.StatusCode;
+import com.sk.api.response.BaseResponse;
+import com.sk.server.dto.KillDto;
+import com.sk.server.service.KillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.HttpSessionRequiredException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import sun.security.provider.certpath.PKIXTimestampParameters;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * 秒杀服务 controller
+ *
+ * @Author yzh
+ * @Date 2020/3/30 17:46
+ * @Version 1.0
+ */
+@Controller
+public class KillController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KillController.class);
+
+    private static final String PREFIX = "kill";
+
+    @Resource
+    private KillService killServiceImpl;
+    /**
+     * 秒杀商品核心业务
+     *
+     * @param dto
+     * @param result
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = PREFIX+"/execute",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public BaseResponse excute(@RequestBody @Validated KillDto dto, BindingResult result, HttpSession session){
+        if(result.hasErrors() || dto.getKillId() <=0) {
+            return new BaseResponse(StatusCode.InvalidParams);
+        }
+        BaseResponse response = new BaseResponse(StatusCode.Success);
+
+        try {
+            Boolean res = killServiceImpl.killItem(dto.getKillId(), dto.getUserId());
+            //不成功
+            if(!res) {
+                return new BaseResponse(StatusCode.Fail.getCode(),"商品已抢购完");
+            }
+        }catch (Exception e ) {
+            response = new BaseResponse(StatusCode.Fail.getCode(),e.getMessage());
+        }
+        return response;
+    }
+}
---
+++ server/src/main/java/com/sk/server/service/impl/KillServiceImpl.java
@@ -0,0 +1,98 @@
+package com.sk.server.service.impl;
+
+import com.sk.model.entity.ItemKill;
+import com.sk.model.entity.ItemKillExample;
+import com.sk.model.entity.ItemKillSuccess;
+import com.sk.model.entity.ItemKillSuccessExample;
+import com.sk.model.mapper.ItemKillMapper;
+import com.sk.model.mapper.ItemKillSuccessMapper;
+import com.sk.server.service.KillService;
+import com.sk.server.utils.RandomUtil;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.swing.text.html.parser.Entity;
+import java.util.Random;
+
+/**
+ * @Author yzh
+ * @Date 2020/3/30 18:24
+ * @Version 1.0
+ */
+@Service
+public class KillServiceImpl implements KillService {
+
+    @Resource
+    private ItemKillSuccessMapper itemKillSuccessMapper;
+
+    @Resource
+    private ItemKillMapper itemKillMapper;
+
+    /**
+     * 商品秒杀处理
+     * @param killId
+     * @param userId
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public Boolean killItem(Integer killId, Integer userId) throws Exception {
+
+        boolean result = false;
+
+        ItemKillSuccessExample example = new ItemKillSuccessExample();
+        example.createCriteria().andUserIdEqualTo(userId+"").andKillIdEqualTo(killId);
+        //判断用户是否已经抢过
+        if(itemKillSuccessMapper.countByExample(example) <= 0) {
+            //查看待秒杀商品详情
+            ItemKill itemKill = itemKillMapper.selectById(killId);
+
+            //判断是否可以被秒杀
+            if(itemKill != null && 1 == itemKill.getCanKill()){
+                //库存减一
+                int res = itemKillMapper.updateItemKillCount(killId);
+                //扣除成功？是生成订单并通知用户
+                if(res>0){
+                    commonRecordKillsuccessInfo(itemKill,userId);
+                    return true;
+                }
+            }
+        }
+        return result;
+    }
+
+    private void commonRecordKillsuccessInfo(ItemKill kill,Integer userid) throws Exception{
+        //记录抢购成功后生成的秒杀订单记录
+        ItemKillSuccess item = new ItemKillSuccess();
+        item.setCode(RandomUtil.generaterOrderCode());
+        item.setKillId(kill.getId());
+        item.setItemId(kill.getItemId());
+        item.setUserId(userid+"");
+        item.setStatus((byte)0);
+        int res = itemKillSuccessMapper.insertSelective(item);
+
+        if(res > 0) {
+            //进行异步发送邮件通知
+        }
+    }
+
+    @Override
+    public Boolean killItemV2(Integer killId, Integer userId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Boolean killItemV3(Integer killId, Integer userId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Boolean killItemV4(Integer killId, Integer userId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Boolean killItemV5(Integer killId, Integer userId) throws Exception {
+        return null;
+    }
+}
